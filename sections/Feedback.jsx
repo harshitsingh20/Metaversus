18ff232822f8c42a655537e23d54bee20c7f2c3e
harshitsingh20 <harshitsingh2035@gmail.com>
2023-04-08 12:35:45 +0530

    Feedback Section updated images and text

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -16,7 +16,8 @@ const Feedback = () => (
     >
       <motion.div
         variants={fadeIn('right', 'tween', 0.2, 1)}
-        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
+        className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px]
+         border-[#6A6A6A] relative"
       >
         <div className="feedback-gradient" />
         <div>
